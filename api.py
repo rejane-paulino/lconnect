3f4472890d411677c318ee2bbca058cd6f1d1f5a
Rejane Paulino <rejane.dspaulino@gmail.com>
2023-06-22 01:56:16 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,18 +1,9 @@
 # -*- processing: LCONNECT -*-
 
 from process.main import Lconnect
-from mode import SATELLITE
 from metadata import ManualParameter
+from mode import SATELLITE
 
-"""
-Code to apply LCONNECT to SuperDove data:
-    :param path_IMG: directory with images;
-    :param path_CLOUD: directory with cloud masks - udm2;
-    :param path_gridPoint: path with file of points (.shp) from reference waterbody (e.g., river);
-    :param path_ROI: path with file of polygons (.shp) from target waterbody (e.g., lakes);
-    :param path_OUTPUT: output directory;
-    :param sensor: applied sensor.
-"""
 
 parameters = ManualParameter()
 sensor = parameters.sensor
